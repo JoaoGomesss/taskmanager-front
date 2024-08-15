e08f8a2fc6b720362562c5165a39e118815f80f1
João Gomes <jowgomes3@gmail.com>
2024-08-15 10:26:13 -0300

    feat: update TaskItem component

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,9 +1,32 @@
+import "./TaskItem.scss";
+import { FaTrashCan } from "react-icons/fa6";
+
 const TaskItem = ({ task }) => {
     return (
-        <>
-            <h1>{task.description}</h1>
-            <p>{task.isCompleted ? "Completa" : "Incompleta"}</p>
-        </>
+        <div className="task-item-container">
+            <div className="task-description">
+                <label
+                    className={
+                        task.isCompleted
+                            ? "checkbox-container-completed "
+                            : "checkbox-container"
+                    }
+                >
+                    {task.description}
+                    <input type="checkbox" defaultChecked={task.isCompleted} />
+                    <span
+                        className={
+                            task.isCompleted
+                                ? "checkmark completed"
+                                : "checkmark"
+                        }
+                    ></span>
+                </label>
+            </div>
+            <div className="delete">
+                <FaTrashCan size={15} color="red" />
+            </div>
+        </div>
     );
 };
 
