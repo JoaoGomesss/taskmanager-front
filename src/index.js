85d5bea50f64b5e8058aafeffcbbceebf1fdbc7b
João Gomes <jowgomes3@gmail.com>
2024-08-15 14:14:52 -0300

    feat: add react-toastify setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import "./index.scss";
 
@@ -10,6 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <App />
+        <ToastContainer />
     </React.StrictMode>
 );
 
