4b5ebaeb62a9f3b335784d83634743a88a308be8
João Gomes <jowgomes3@gmail.com>
2024-08-15 10:05:57 -0300

    feat: add TaskItem component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Task from "./components/Tasks";
+
+const App = () => {
+    return (
+        <>
+            <Task />
+        </>
+    );
+};
+
+export default App;
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import "./Tasks.scss";
+import TaskItem from "./TaskItem";
+
+const Task = () => {
+    const [tasks, setTasks] = useState([]);
+
+    const fetchTasks = async () => {
+        try {
+            const { data } = await axios.get(
+                "https://taskmanager-vq5u.onrender.com/tasks"
+            );
+            setTasks(data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchTasks();
+    }, []);
+
+    return (
+        <div className="tasks-container">
+            <h2>Minhas tarefas</h2>
+
+            <div className="last-tasks">
+                <h3>Últimas tarefas</h3>
+                <div className="tasks-list">
+                    {tasks
+                        .filter((task) => task.isCompleted === false)
+                        .map((lastTask) => (
+                            <p>{lastTask.description}</p>
+                        ))}
+                </div>
+            </div>
+
+            <div className="completed-tasks">
+                <h3>Tarefas Concluídas</h3>
+                <div className="tasks-list">
+                    {tasks
+                        .filter((task) => task.isCompleted)
+                        .map((concludedTask) => (
+                            <p>{concludedTask.description}</p>
+                        ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Task;
