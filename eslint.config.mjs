6ccde8415b51fe5856b39fcc67d37f1324f85def
João Gomes <jowgomes3@gmail.com>
2024-08-16 14:56:49 -0300

    fix: config eslint and husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,10 +2,29 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 
-
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
+    { files: ["**/*.{js,mjs,cjs,jsx}"] },
+    { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+    pluginJs.configs.recommended,
+    pluginReact.configs.flat.recommended,
+    {
+        env: {
+            browser: true,
+            es2021: true,
+            node: true,
+        },
+        extends: ["plugin:react/recommended", "standard"],
+        parserOptions: {
+            ecmaFeatures: {
+                jsx: true,
+            },
+            ecmaVersion: "latest",
+            sourceType: "module",
+        },
+        plugins: ["react"],
+        rules: {
+            "react/react-in-jsx-scope": "off",
+            "react/prop-types": "off",
+        },
+    },
 ];
